6a986cece1aac686cce63affb7b61ac99f9dd4ac
IMP1 <huw_taylor@hotmail.co.uk>
2022-03-18 13:03:01 +0000

    Add music manager to Main

--- project/scenes/Main.gd
+++ project/scenes/Main.gd
@@ -4,3 +4,7 @@ func transition_scene(next_scene_path: String):
 	var next_scene = load(next_scene_path).instance()
 	$CurrentScene.get_child(0).queue_free()
 	$CurrentScene.add_child(next_scene)
+	current_scene = next_scene
+
+onready var music_manager = $MusicManager
+onready var current_scene = $CurrentScene.get_child(0)
